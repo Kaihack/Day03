29ea6e885abdf593872389650fbc3f5263b5c6ba
Kaihack <prostor.za.rad@gmail.com>
2021-03-10 14:26:46 +0100

    Added index.js file with comments

---
+++ ex02/src/index.js
@@ -0,0 +1,9 @@
+function main() {
+    // Only change code below this line
+//console.log('Hello world!'); single line comment
+    // Only change code above this line
+}
+/*main();
+
+module.exports = main; multi-line comment
+*/
